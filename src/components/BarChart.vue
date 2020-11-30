7aa3bb62966107a7e00f737839d33593291f3843
d.kelly@maxforham.com <dan>
2020-11-30 11:29:27 +0000

    Working chartjs example

---
+++ src/components/BarChart.vue
@@ -0,0 +1,14 @@
+<script>
+import { Bar, mixins } from 'vue-chartjs'
+
+const { reactiveProp } = mixins
+
+export default {
+  extends: Bar,
+  mixins: [reactiveProp],
+  props: ['data', 'options'],
+  mounted () {
+    this.renderChart(this.data, this.options)
+  }
+}
+</script>
